05613f13f8c7cf07bec2fbcb9cd0125baeab7a5d
root <root@localhost.localdomain>
2018-10-17 00:59:19 -0400

    Algorithm Tweaking & Enhancement

--- network_scanner.py
+++ network_scanner.py
@@ -4,8 +4,15 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
 # end scan
 
 
 scan("10.0.2.1/24")
+# Use ARP to ask for IP
+
+
+
+# Set destination MAC to broadcast MAC
