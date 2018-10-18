2a57c241481987a49c94f6f5fa6bf53decc6fe70
root <root@localhost.localdomain>
2018-10-18 19:16:36 -0400

    Send and Receive packets initial code

--- network_scanner.py
+++ network_scanner.py
@@ -7,12 +7,13 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 # end scan
 
 
 scan("10.0.2.1/24")
+
 # Use ARP to ask for IP
-
-
 
 # Set destination MAC to broadcast MAC
