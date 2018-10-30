8cc1bf51ff2371eb7eca8ed249a11d872a69d8c4
root <root@localhost.localdomain>
2018-10-30 02:26:24 -0400

    Learning optparse vs. argparse, new functions, finalizing program

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,15 @@
-#!/usr/bin.env python
+#!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.addargument("-t", "--target", dest="target", help="Target IP / IP Range")
+    options = parser.parse_args()
+    return options
+# end get_arguments
 
 
 def scan(ip):
@@ -9,10 +18,21 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("IP\t\t\tMAC Address\n------------------------------------")
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 # end scan
 
 
-scan("10.0.2.1/24")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+# end print_result
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
